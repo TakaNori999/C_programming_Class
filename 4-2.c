cc76617e667ec5f53023afcbc51f43cee5a9c434
taka <59133507+TakaNori999@users.noreply.github.com>
2026-04-27 08:27:42 +0900

    Fix date input and output format

--- 4-2.c
+++ 4-2.c
@@ -2,7 +2,7 @@
 int main(void){
     char i[4] = {0};
     printf("Enter today's date = ");
-    scanf("%s", &i);
-    printf("Today is %s.", i);
+    scanf("%s", i);
+    printf("Today is %s.\n", i);
     return 0;
 }
